b9c9833e785d9c3a86a3a85f40cea1bbbd940efe
wilfredloh <lohwilfred19@gmail.com>
2019-08-20 09:47:00 +0800

    updated yarn

--- db/schema.rb
+++ db/schema.rb
@@ -15,13 +15,6 @@ ActiveRecord::Schema.define(version: 2019_08_19_114422) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "templates", force: :cascade do |t|
-    t.string "column1"
-    t.text "column2"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Contacts.create(name: 'Eric', phone: '12345678', company: 'Google', email: 'eric@google.com', position: 'Recruiter')
+Contacts.create(name: 'Susan', phone: '98765432', company: 'Facebook', email: 'susan@facebook.com', position: 'HR')
+Contacts.create(name: 'John', phone: '88888888', company: 'Apple', email: 'john@apple.com', position: 'Recruiter')
+Contacts.create(name: 'Sandy', phone: '09090909', company: 'Yahoo', email: 'sandy@yahoo.com', position: 'Manager')
+Contacts.create(name: 'Sam', phone: '99999999', company: 'Airbnb', email: 'sam@airbnb.com', position: 'Executive')
+Contacts.create(name: 'Samantha', phone: '14561234', company: 'Grab', email: 'samantha@grab.com', position: 'Recruiter')
