db51afd67a00047280e54fa49124ad5bc69ba971
wilfredloh <lohwilfred19@gmail.com>
2019-08-20 14:17:51 +0800

    added home view

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,43 +1,62 @@
 class JobsController < ApplicationController
 before_action :authenticate_user!, :except => [ :show, :index, :create, :destroy ]
-    def new
-      @job = Job.new
+
+    def home
+      @jobs = Job.all
+      @added = @jobs.where(status:"Saved").length
+      @applied = @jobs.where(status:"Submitted").length
+      @offer = @jobs.where(status:"Offer").length
+      @rejected = @jobs.where(status:"Rejected").length
+
+      count = 0
+      @jobs.each do |job|
+        if job.created_at.to_date == Time.now.to_date
+          count += 1
+        end
+      end
+      @count = count
+
+      @deadline = @jobs.sort_by{|job| job.deadline}
     end
 
     def index
       @jobs = Job.all
     end
 
-  def create
-    @job = Job.new(job_params)
-    @job.user = current_user
-    @job.save
-    redirect_to root_path
-  end
+    def new
+      @job = Job.new
+    end
 
-  def edit
-    @job = Job.find(params[:id])
-  end
+    def create
+      @job = Job.new(job_params)
+      @job.user = current_user
+      @job.save
+      redirect_to root_path
+    end
 
-  def update
-    @job = Job.find(params[:id])
-    @job.update(job_params)
-    redirect_to root_path
-  end
+    def edit
+      @job = Job.find(params[:id])
+    end
 
-  def show
-    @job = Job.find(params[:id])
-  end
+    def update
+      @job = Job.find(params[:id])
+      @job.update(job_params)
+      redirect_to root_path
+    end
+
+    def show
+      @job = Job.find(params[:id])
+    end
 
     def destroy
-    @job = Job.find(params[:id])
-    @job.destroy
-    redirect_to @job
-  end
+      @job = Job.find(params[:id])
+      @job.destroy
+      redirect_to @job
+    end
 
 private
 
-  def job_params
-    params.require(:job).permit(:comp_name, :title, :location, :salary, :url, :deadline)
-  end
+    def job_params
+      params.require(:job).permit(:comp_name, :title, :location, :salary, :url, :deadline)
+    end
 end
